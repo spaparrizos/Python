adf92bfa065e7bea53c583628a6623e90e7cf9c7
Spyridon Paparrizos <spipap@gmail.com>
2018-01-20 00:15:03 +0100

    Update 02_Kobayashi.py

--- 02_Kobayashi.py
+++ 02_Kobayashi.py
@@ -4,7 +4,7 @@
 # spyridon.paparrizos@lsce.ipsl.fr                     
 ########################################################
 
-# The Kobayashi approach using Python on comparing simulated and measured values using mean squared deviation and its components
+# The Kobayashi approach using Python on comparing simulated and observed values using mean squared deviation and its components
 # Kobayashi K, Salam MU, 2000. Comparing Simulated and Measured Values Using Mean Squared Deviation and its Components, Journal 
 #    of Agronomy, 92, 345-352. https://dl.sciencesocieties.org/publications/aj/abstracts/92/2/345?access=0&view=pdf  
 
@@ -14,7 +14,7 @@ x = np.ma.array(np.random.random(100))			# 1st testing dataset
 y = np.ma.array(np.random.random(100))			# 2nd testing dataset
 
 # Kobayashi formula
-def compare(x, y):
+def Kobayashi(x, y):
     xmean = x.mean(axis=0) 													
     ymean = y.mean(axis=0) 													
     SB = (xmean - ymean)**2
@@ -26,13 +26,13 @@ def compare(x, y):
     if (1-r) < 0: print r; raw_input("NAN")
     return SB, SDSD, LCS
 
-SB, SDSD, LCS = compare(x, y)
+SB, SDSD, LCS = Kobayashi(x, y)
 MSD = SB+SDSD+LCS
 
 # Additional check whether the Kobayashi formula was correctly applied (MSD == RMSE**2)
 
-def calcRMSE(x, y): 				# x and y need to have the same dimensions
+def RMSE(x, y): 				# x and y need to have the same dimensions
     return ( ( (x - y)**2 ).mean(axis=0) )**0.5
     
-RMSE = calcRMSE(x,y)
+RMSE = RMSE(x,y)
 print "MSD (SB+SDSD+LCS) - RMSE**2 = 0, and here it is: ",(SB+SDSD+LCS - RMSE**2).sum()
