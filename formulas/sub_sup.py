22d99dbc609d51972ce4c699bf6ba5bf6f4f623c
Spyridon Paparrizos <spipap@gmail.com>
2019-02-25 16:33:54 +0100

    create sub_sup.py
    @ Python formula to convert text into subscript or superscript

---
+++ formulas/sub_sup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#########################################################
+# Spyros Paparrizos											       
+# spipap@gmail.com       				            
+# Superscript + Subscript	`								       
+#########################################################
+
+# Python libraries
+import numpy as np, netCDF4, os, sys
+
+
+# Subscript - Superscript:
+print '$\mathregular{CH_{text}^{4}}$'
+plt.xlabel('$\mathregular{CH_{text}^{4}}$')
+
